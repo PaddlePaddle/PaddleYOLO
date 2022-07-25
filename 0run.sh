76529ccfce631a914646e5ae9fbe3b8d30ed5d86
nemonameless <nemonameless@qq.com>
2022-07-25 03:05:50 +0000

    add base code, l x infer

---
+++ 0run.sh
@@ -0,0 +1,36 @@
+export FLAGS_allocator_strategy=auto_growth
+#name=tiny
+name=l
+model_type=yolov7
+job_name=yolov7_${name}_300e_coco
+config=configs/${model_type}/${job_name}.yml
+log_dir=log_dir/${job_name}
+weights=https://bj.bcebos.com/v1/paddledet/models/yolov5_${name}_300e_coco.pdparams
+
+weights=../yolov7_tools/yolov7_${name}_300e_coco.pdparams
+
+#python3.7 dygraph_print.py -c ${config} 2>&1 | tee yolov7_${name}_dy_print.txt
+#python3.7 4convert_torch2paddle.py yolov7.pt weight_name_map_l.txt yolov7.pdparams
+#python3.7 4convert_torch2paddle.py yolov7x.pt weight_name_map_x.txt yolov7x.pdparams
+
+# 1. training
+#CUDA_VISIBLE_DEVICES=7 python3.7 tools/train.py -c ${config} -r ${weights}
+#python3.7 -m paddle.distributed.launch --log_dir=${log_dir} --gpus 4,5,6,7 tools/train.py -c ${config} --eval --amp
+
+# 2. eval
+#CUDA_VISIBLE_DEVICES=0 python3.7 tools/eval.py -c ${config} -o weights=https://paddledet.bj.bcebos.com/models/${job_name}.pdparams
+#CUDA_VISIBLE_DEVICES=7 python3.7 tools/eval.py -c ${config} -o weights=${weights}
+
+# 3. tools infer
+#CUDA_VISIBLE_DEVICES=7 python3.7 tools/infer.py -c ${config} -o weights=https://paddledet.bj.bcebos.com/models/${job_name}.pdparams --infer_img=demo/000000014439.jpg
+CUDA_VISIBLE_DEVICES=7 python3.7 tools/infer.py -c ${config} -o weights=${weights} --infer_img=demo/000000014439_640x640.jpg
+
+# 4.export model
+#CUDA_VISIBLE_DEVICES=7 python3.7 tools/export_model.py -c ${config} -o weights=https://paddledet.bj.bcebos.com/models/${job_name}.pdparams
+#CUDA_VISIBLE_DEVICES=7 python3.7 tools/export_model.py -c ${config} -o weights=${weights}
+
+# 5. deploy infer
+#CUDA_VISIBLE_DEVICES=7 python3.7 deploy/python/infer.py --model_dir=output_inference/${job_name} --image_file=demo/000000014439_640x640.jpg --device=GPU
+
+# 6. deploy speed
+#CUDA_VISIBLE_DEVICES=7 python3.7 deploy/python/infer.py --model_dir=output_inference/${job_name} --image_file=demo/000000014439_640x640.jpg --device=GPU #--run_benchmark=True --run_mode=trt_fp16
--- ppdet/modeling/heads/yolo_head.py
+++ ppdet/modeling/heads/yolo_head.py
@@ -28,7 +28,7 @@ from ppdet.modeling.assigners.simota_assigner import SimOTAAssigner
 from ppdet.modeling.bbox_utils import bbox_overlaps
 from ppdet.modeling.layers import MultiClassNMS
 
-__all__ = ['YOLOv3Head', 'YOLOXHead', 'YOLOv5Head']
+__all__ = ['YOLOv3Head', 'YOLOXHead', 'YOLOv5Head', 'YOLOv7Head']
 
 
 def _de_sigmoid(x, eps=1e-7):
@@ -199,7 +199,8 @@ class YOLOXHead(nn.Layer):
             self.conv_cls.append(
                 nn.Sequential(* [
                     ConvBlock(
-                        feat_channels, feat_channels, 3, 1, act=act), ConvBlock(
+                        feat_channels, feat_channels, 3, 1,
+                        act=act), ConvBlock(
                             feat_channels, feat_channels, 3, 1, act=act),
                     nn.Conv2D(
                         feat_channels,
@@ -246,7 +247,9 @@ class YOLOXHead(nn.Layer):
             x = (paddle.arange(w) + offset) * stride
             y = (paddle.arange(h) + offset) * stride
             y, x = paddle.meshgrid(y, x)
-            anchor_points.append(paddle.stack([x, y], axis=-1).reshape([-1, 2]))
+            anchor_points.append(
+                paddle.stack(
+                    [x, y], axis=-1).reshape([-1, 2]))
             stride_tensor.append(
                 paddle.full(
                     [len(anchor_points[-1]), 1], stride, dtype=self._dtype))
@@ -554,8 +557,175 @@ class YOLOv5Head(nn.Layer):
                     [0, 1, 3, 4, 2])
             # head_out.shape [bs, self.num_anchor, ny, nx, self.num_out_ch]
 
-            bbox, score = self.postprocessing_by_level(head_out, self.stride[i],
-                                                       self.anchors[i], ny, nx)
+            bbox, score = self.postprocessing_by_level(
+                head_out, self.stride[i], self.anchors[i], ny, nx)
+            bbox = bbox.reshape([bs, self.num_anchor * ny * nx, 4])
+            score = score.reshape(
+                [bs, self.num_anchor * ny * nx, self.num_classes]).transpose(
+                    [0, 2, 1])
+            bbox_list.append(bbox)
+            score_list.append(score)
+        pred_bboxes = paddle.concat(bbox_list, axis=1)
+        pred_scores = paddle.concat(score_list, axis=-1)
+        scale_factor = scale_factor.flip(-1).tile([1, 2]).unsqueeze(1)
+        pred_bboxes /= scale_factor
+
+        if self.exclude_nms:
+            # `exclude_nms=True` just use in benchmark for speed test
+            return paddle.concat(
+                [pred_bboxes, pred_scores.transpose([0, 2, 1])],
+                axis=-1), paddle.to_tensor(
+                    [1], dtype='int32')
+        else:
+            bbox_pred, bbox_num, _ = self.nms(pred_bboxes, pred_scores)
+            return bbox_pred, bbox_num
+
+
+from IPython import embed
+
+
+@register
+class YOLOv7Head(nn.Layer):
+    __shared__ = ['num_classes', 'data_format', 'trt', 'exclude_nms']
+    __inject__ = ['loss', 'nms']
+
+    def __init__(self,
+                 num_classes=80,
+                 in_channels=[256, 512, 1024],
+                 anchors=[[12, 16], [19, 36], [40, 28], [36, 75], [76, 55],
+                          [72, 146], [142, 110], [192, 243], [459, 401]],
+                 anchor_masks=[[0, 1, 2], [3, 4, 5], [6, 7, 8]],
+                 stride=[8, 16, 32],
+                 loss='YOLOv5Loss',
+                 data_format='NCHW',
+                 nms='MultiClassNMS',
+                 trt=False,
+                 exclude_nms=False):
+        """
+        Head for YOLOv7
+
+        Args:
+            num_classes (int): number of foreground classes
+            in_channels (int): channels of input features
+            anchors (list): anchors
+            anchor_masks (list): anchor masks
+            stride (list): strides
+            loss (object): YOLOv5Loss instance
+            data_format (str): nms format, NCHW or NHWC
+            loss (object): YOLOv5Loss instance
+        """
+        super(YOLOv7Head, self).__init__()
+        assert len(in_channels) > 0, "in_channels length should > 0"
+        self.num_classes = num_classes
+        self.in_channels = in_channels
+
+        self.parse_anchor(anchors, anchor_masks)
+        self.anchors = paddle.to_tensor(self.anchors, dtype='float32')
+        self.anchor_levels = len(self.anchors)
+
+        self.stride = stride
+        self.loss = loss
+        self.data_format = data_format
+        self.nms = nms
+        if isinstance(self.nms, MultiClassNMS) and trt:
+            self.nms.trt = trt
+        self.exclude_nms = exclude_nms
+
+        self.num_anchor = len(self.anchors[0])  # self.na
+        self.num_out_ch = self.num_classes + 5  # self.no
+
+        self.yolo_outputs = []
+        for i in range(len(self.anchors)):
+            num_filters = self.num_anchor * self.num_out_ch
+            name = 'yolo_output.{}'.format(i)
+            conv = nn.Conv2D(
+                in_channels=self.in_channels[i],
+                out_channels=num_filters,
+                kernel_size=1,
+                stride=1,
+                padding=0,
+                data_format=data_format,
+                bias_attr=ParamAttr(regularizer=L2Decay(0.)))
+            conv.skip_quant = True
+            yolo_output = self.add_sublayer(name, conv)
+            self.yolo_outputs.append(yolo_output)
+
+        #self._initialize_biases()
+
+    def _initialize_biases(self):
+        # initialize biases into Detect()
+        # https://arxiv.org/abs/1708.02002 section 3.3
+        import math
+        for i, conv in enumerate(self.yolo_outputs):
+            b = conv.bias.numpy().reshape([3, -1])
+            b[:, 4] += math.log(8 / (640 / self.stride[i])**2)
+            b[:, 5:] += math.log(0.6 / (self.num_classes - 0.999999))
+            conv.bias.set_value(b.reshape([-1]))
+
+    def parse_anchor(self, anchors, anchor_masks):
+        self.anchors = [[anchors[i] for i in mask] for mask in anchor_masks]
+        self.mask_anchors = []
+        anchor_num = len(anchors)
+        for masks in anchor_masks:
+            self.mask_anchors.append([])
+            for mask in masks:
+                assert mask < anchor_num, "anchor mask index overflow"
+                self.mask_anchors[-1].extend(anchors[mask])
+
+    def forward(self, feats, targets=None):
+        assert len(feats) == len(self.anchors)
+        yolo_outputs = []
+        for i, feat in enumerate(feats):
+            yolo_output = self.yolo_outputs[i](feat)
+            if self.data_format == 'NHWC':
+                yolo_output = paddle.transpose(yolo_output, [0, 3, 1, 2])
+            yolo_outputs.append(yolo_output)
+
+        if self.training:
+            return self.loss(yolo_outputs, targets, self.anchors)
+        else:
+            return yolo_outputs
+
+    @classmethod
+    def from_config(cls, cfg, input_shape):
+        return {'in_channels': [i.channels for i in input_shape], }
+
+    def make_grid(self, nx, ny, anchor):
+        yv, xv = paddle.meshgrid([
+            paddle.arange(
+                ny, dtype='float32'), paddle.arange(
+                    nx, dtype='float32')
+        ])
+
+        grid = paddle.stack(
+            (xv, yv), axis=2).expand([1, self.num_anchor, ny, nx, 2])
+        anchor_grid = anchor.reshape([1, self.num_anchor, 1, 1, 2]).expand(
+            (1, self.num_anchor, ny, nx, 2))
+        return grid, anchor_grid
+
+    def postprocessing_by_level(self, head_out, stride, anchor, ny, nx):
+        grid, anchor_grid = self.make_grid(nx, ny, anchor)
+        out = F.sigmoid(head_out)
+        xy = (out[..., 0:2] * 2. - 0.5 + grid) * stride
+        wh = (out[..., 2:4] * 2)**2 * anchor_grid
+        lt_xy = (xy - wh / 2.)
+        rb_xy = (xy + wh / 2.)
+        bboxes = paddle.concat((lt_xy, rb_xy), axis=-1)
+        scores = out[..., 5:] * out[..., 4].unsqueeze(-1)
+        return bboxes, scores
+
+    def post_process(self, head_outs, img_shape, scale_factor):
+        bbox_list, score_list = [], []
+
+        for i, head_out in enumerate(head_outs):
+            bs, _, ny, nx = head_out.shape
+            head_out = head_out.reshape(
+                [bs, self.num_anchor, self.num_out_ch, ny, nx]).transpose(
+                    [0, 1, 3, 4, 2])
+            # head_out.shape [bs, self.num_anchor, ny, nx, self.num_out_ch]
+
+            bbox, score = self.postprocessing_by_level(
+                head_out, self.stride[i], self.anchors[i], ny, nx)
             bbox = bbox.reshape([bs, self.num_anchor * ny * nx, 4])
             score = score.reshape(
                 [bs, self.num_anchor * ny * nx, self.num_classes]).transpose(
