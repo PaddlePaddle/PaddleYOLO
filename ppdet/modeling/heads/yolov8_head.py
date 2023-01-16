0690e5978879c168f358168fbbd1901bdc7cbea6
nemonameless <nemonameless@qq.com>
2023-01-16 14:50:01 +0000

    fix yolov8head reshape and refine codes

--- ppdet/modeling/heads/yolov8_head.py
+++ ppdet/modeling/heads/yolov8_head.py
@@ -33,7 +33,8 @@ __all__ = ['YOLOv8Head']
 @register
 class YOLOv8Head(nn.Layer):
     __shared__ = [
-        'num_classes', 'eval_size', 'trt', 'exclude_nms', 'exclude_post_process'
+        'num_classes', 'eval_size', 'trt', 'exclude_nms',
+        'exclude_post_process', 'use_shared_conv'
     ]
     __inject__ = ['assigner', 'nms']
 
@@ -45,6 +46,7 @@ class YOLOv8Head(nn.Layer):
                  grid_cell_scale=5.0,
                  grid_cell_offset=0.5,
                  reg_max=16,
+                 reg_range=None,
                  use_varifocal_loss=False,
                  assigner='TaskAlignedAssigner',
                  nms='MultiClassNMS',
@@ -57,6 +59,7 @@ class YOLOv8Head(nn.Layer):
                  trt=False,
                  exclude_nms=False,
                  exclude_post_process=False,
+                 use_shared_conv=True,
                  print_l1_loss=True):
         super(YOLOv8Head, self).__init__()
         assert len(in_channels) > 0, "len(in_channels) should > 0"
@@ -65,7 +68,11 @@ class YOLOv8Head(nn.Layer):
         self.fpn_strides = fpn_strides
         self.grid_cell_scale = grid_cell_scale
         self.grid_cell_offset = grid_cell_offset
-        self.reg_max = reg_max
+        if reg_range:
+            self.reg_range = reg_range
+        else:
+            self.reg_range = (0, reg_max)  # not reg_max+1
+        self.reg_channels = self.reg_range[1] - self.reg_range[0]
         self.use_varifocal_loss = use_varifocal_loss
         self.assigner = assigner
         self.nms = nms
@@ -76,13 +83,14 @@ class YOLOv8Head(nn.Layer):
         self.exclude_nms = exclude_nms
         self.exclude_post_process = exclude_post_process
         self.print_l1_loss = print_l1_loss
+        self.use_shared_conv = use_shared_conv
 
         # cls loss
         self.bce = nn.BCEWithLogitsLoss(
             pos_weight=paddle.to_tensor([1.0]), reduction="mean")
 
         # pred head
-        c2 = max((16, in_channels[0] // 4, self.reg_max * 4))
+        c2 = max((16, in_channels[0] // 4, self.reg_channels * 4))
         c3 = max(in_channels[0], self.num_classes)
         self.conv_reg = nn.LayerList()
         self.conv_cls = nn.LayerList()
@@ -95,7 +103,7 @@ class YOLOv8Head(nn.Layer):
                         c2, c2, 3, 1, act=act),
                     nn.Conv2D(
                         c2,
-                        self.reg_max * 4,
+                        self.reg_channels * 4,
                         1,
                         bias_attr=ParamAttr(regularizer=L2Decay(0.0))),
                 ]))
@@ -112,11 +120,11 @@ class YOLOv8Head(nn.Layer):
                         bias_attr=ParamAttr(regularizer=L2Decay(0.0))),
                 ]))
         # projection conv
-        self.dfl_conv = nn.Conv2D(self.reg_max, 1, 1, bias_attr=False)
+        self.dfl_conv = nn.Conv2D(self.reg_channels, 1, 1, bias_attr=False)
         self.dfl_conv.skip_quant = True
-        self.proj = paddle.linspace(0, self.reg_max - 1, self.reg_max)
+        self.proj = paddle.linspace(0, self.reg_channels - 1, self.reg_channels)
         self.dfl_conv.weight.set_value(
-            self.proj.reshape([1, self.reg_max, 1, 1]))
+            self.proj.reshape([1, self.reg_channels, 1, 1]))
         self.dfl_conv.weight.stop_gradient = True
 
         # self._init_bias()
@@ -143,39 +151,53 @@ class YOLOv8Head(nn.Layer):
                 feats, self.fpn_strides, self.grid_cell_scale,
                 self.grid_cell_offset)
 
-        cls_list, reg_list = [], []
+        cls_score_list, reg_distri_list = [], []
         for i, feat in enumerate(feats):
-            pred_reg = self.conv_reg[i](feat)
-            pred_cls = self.conv_cls[i](feat)
-            reg_list.append(pred_reg.flatten(2).transpose([0, 2, 1]))
-            cls_list.append(pred_cls.flatten(2).transpose([0, 2, 1]))
-        cls_concat = paddle.concat(cls_list, axis=1)
-        reg_concat = paddle.concat(reg_list, axis=1)
+            reg_distri = self.conv_reg[i](feat)
+            cls_logit = self.conv_cls[i](feat)
+            # cls and reg
+            cls_score = F.sigmoid(cls_logit)
+            cls_score_list.append(cls_score.flatten(2).transpose([0, 2, 1]))
+            reg_distri_list.append(reg_distri.flatten(2).transpose([0, 2, 1]))
+        cls_score_list = paddle.concat(cls_score_list, axis=1)
+        reg_distri_list = paddle.concat(reg_distri_list, axis=1)
+
         return self.get_loss([
-            cls_concat, reg_concat, anchors, anchor_points, num_anchors_list,
-            stride_tensor
+            cls_score_list, reg_distri_list, anchors, anchor_points,
+            num_anchors_list, stride_tensor
         ], targets)
 
     def forward_eval(self, feats):
         anchor_points, stride_tensor = self._generate_anchors(feats)
 
-        cls_list, reg_list = [], []
+        cls_score_list, reg_dist_list = [], []
         for i, feat in enumerate(feats):
-            b, _, h, w = feat.shape
+            _, _, h, w = feat.shape
             l = h * w
-            pred_reg = self.conv_reg[i](feat)
-            pred_cls = self.conv_cls[i](feat)
+            reg_dist = self.conv_reg[i](feat)
+            cls_logit = self.conv_cls[i](feat)
 
-            pred_reg = pred_reg.reshape([-1, 4, self.reg_max, l]).transpose(
-                [0, 2, 1, 3])
-            pred_reg = self.dfl_conv(F.softmax(pred_reg, axis=1))
-            cls_score = F.sigmoid(pred_cls)
+            reg_dist = reg_dist.reshape(
+                [-1, 4, self.reg_channels, l]).transpose(
+                    [0, 2, 3, 1])  # Note diff
+            if self.use_shared_conv:
+                reg_dist = self.dfl_conv(F.softmax(reg_dist, axis=1)).squeeze(1)
+                # [bs, l, 4]
+            else:
+                reg_dist = F.softmax(reg_dist, axis=1)
+            # cls and reg
+            cls_score = F.sigmoid(cls_logit)
+            cls_score_list.append(cls_score.reshape([-1, self.num_classes, l]))
+            reg_dist_list.append(reg_dist)
 
-            cls_list.append(cls_score.reshape([b, self.num_classes, l]))
-            reg_list.append(pred_reg.reshape([b, 4, l]))
-        cls_concat = paddle.concat(cls_list, axis=-1)
-        reg_concat = paddle.concat(reg_list, axis=-1)
-        return cls_concat, reg_concat, anchor_points, stride_tensor
+        cls_score_list = paddle.concat(cls_score_list, axis=-1)
+        if self.use_shared_conv:
+            reg_dist_list = paddle.concat(reg_dist_list, axis=1)
+        else:
+            reg_dist_list = paddle.concat(reg_dist_list, axis=2)
+            reg_dist_list = self.dfl_conv(reg_dist_list).squeeze(1)
+
+        return cls_score_list, reg_dist_list, anchor_points, stride_tensor
 
     def _generate_anchors(self, feats=None, dtype='float32'):
         # just use in eval time
@@ -200,16 +222,6 @@ class YOLOv8Head(nn.Layer):
         return anchor_points, stride_tensor
 
     @staticmethod
-    def _focal_loss(score, label, alpha=0.25, gamma=2.0):
-        weight = (score - label).pow(gamma)
-        if alpha > 0:
-            alpha_t = alpha * label + (1 - alpha) * (1 - label)
-            weight *= alpha_t
-        loss = F.binary_cross_entropy(
-            score, label, weight=weight, reduction='sum')
-        return loss
-
-    @staticmethod
     def _varifocal_loss(pred_score, gt_score, label, alpha=0.75, gamma=2.0):
         weight = alpha * pred_score.pow(gamma) * (1 - label) + gt_score * label
         loss = F.binary_cross_entropy(
@@ -217,26 +229,29 @@ class YOLOv8Head(nn.Layer):
         return loss
 
     def _bbox_decode(self, anchor_points, pred_dist):
-        b, l, _ = get_static_shape(pred_dist)
-        pred_dist = F.softmax(pred_dist.reshape([b, l, 4, self.reg_max
-                                                 ])).matmul(self.proj)
+        _, l, _ = get_static_shape(pred_dist)
+        pred_dist = F.softmax(pred_dist.reshape([-1, l, 4, self.reg_channels]))
+        pred_dist = self.dfl_conv(pred_dist.transpose([0, 3, 1, 2])).squeeze(1)
         return batch_distance2bbox(anchor_points, pred_dist)
 
-    def _bbox2distance(self, points, bbox, reg_max):
+    def _bbox2distance(self, points, bbox):
         x1y1, x2y2 = paddle.split(bbox, 2, -1)
         lt = points - x1y1
         rb = x2y2 - points
-        return paddle.concat([lt, rb], -1).clip(0, reg_max - 0.01)
+        return paddle.concat([lt, rb], -1).clip(self.reg_range[0],
+                                                self.reg_range[1] - 1 - 0.01)
 
-    def _df_loss(self, pred_dist, target):
-        target_left = paddle.cast(target, 'int64')
+    def _df_loss(self, pred_dist, target, lower_bound=0):
+        target_left = paddle.cast(target.floor(), 'int64')
         target_right = target_left + 1
         weight_left = target_right.astype('float32') - target
         weight_right = 1 - weight_left
         loss_left = F.cross_entropy(
-            pred_dist, target_left, reduction='none') * weight_left
+            pred_dist, target_left - lower_bound,
+            reduction='none') * weight_left
         loss_right = F.cross_entropy(
-            pred_dist, target_right, reduction='none') * weight_right
+            pred_dist, target_right - lower_bound,
+            reduction='none') * weight_right
         return (loss_left + loss_right).mean(-1, keepdim=True)
 
     def _bbox_loss(self, pred_dist, pred_bboxes, anchor_points, assigned_labels,
@@ -267,15 +282,14 @@ class YOLOv8Head(nn.Layer):
             loss_iou = ((1.0 - iou) * bbox_weight).sum() / assigned_scores_sum
 
             dist_mask = mask_positive.unsqueeze(-1).tile(
-                [1, 1, (self.reg_max) * 4])
+                [1, 1, self.reg_channels * 4])
             pred_dist_pos = paddle.masked_select(
-                pred_dist, dist_mask).reshape([-1, 4, self.reg_max])
-            assigned_ltrb = self._bbox2distance(anchor_points, assigned_bboxes,
-                                                self.reg_max - 1)
+                pred_dist, dist_mask).reshape([-1, 4, self.reg_channels])
+            assigned_ltrb = self._bbox2distance(anchor_points, assigned_bboxes)
             assigned_ltrb_pos = paddle.masked_select(
                 assigned_ltrb, bbox_mask).reshape([-1, 4])
-            loss_dfl = self._df_loss(pred_dist_pos,
-                                     assigned_ltrb_pos) * bbox_weight
+            loss_dfl = self._df_loss(pred_dist_pos, assigned_ltrb_pos,
+                                     self.reg_range[0]) * bbox_weight
             loss_dfl = loss_dfl.sum() / assigned_scores_sum
         else:
             loss_l1 = paddle.zeros([1])
@@ -293,6 +307,7 @@ class YOLOv8Head(nn.Layer):
         gt_labels = gt_meta['gt_class']
         gt_bboxes = gt_meta['gt_bbox']
         pad_gt_mask = gt_meta['pad_gt_mask']
+        # label assignment
         assigned_labels, assigned_bboxes, assigned_scores = \
             self.assigner(
             pred_scores.detach(),
@@ -317,9 +332,8 @@ class YOLOv8Head(nn.Layer):
         assigned_scores_sum = assigned_scores.sum()
         if paddle.distributed.get_world_size() > 1:
             paddle.distributed.all_reduce(assigned_scores_sum)
-            assigned_scores_sum = paddle.clip(
-                assigned_scores_sum / paddle.distributed.get_world_size(),
-                min=1)
+            assigned_scores_sum /= paddle.distributed.get_world_size()
+        assigned_scores_sum = paddle.clip(assigned_scores_sum, min=1.)
         # loss_cls /= assigned_scores_sum
 
         loss_l1, loss_iou, loss_dfl = \
@@ -341,9 +355,8 @@ class YOLOv8Head(nn.Layer):
         return out_dict
 
     def post_process(self, head_outs, im_shape, scale_factor):
-        pred_scores, reg_concat, anchor_points, stride_tensor = head_outs
-        pred_bboxes = batch_distance2bbox(anchor_points,
-                                          reg_concat.transpose([0, 2, 1]))
+        pred_scores, pred_dist, anchor_points, stride_tensor = head_outs
+        pred_bboxes = batch_distance2bbox(anchor_points, pred_dist)
         pred_bboxes *= stride_tensor
         if self.exclude_post_process:
             return paddle.concat(
