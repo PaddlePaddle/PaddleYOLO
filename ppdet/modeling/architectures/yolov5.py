a417ca2ed41a7f71fb7a0c2d2836b1c592023c9a
nemonameless@qq.com@github.com <nemonameless@qq.com>
2023-09-11 05:46:30 +0000

    fix yolov5u infer

--- ppdet/modeling/architectures/yolov5.py
+++ ppdet/modeling/architectures/yolov5.py
@@ -78,7 +78,9 @@ class YOLOv5(BaseArch):
             return yolo_losses
         else:
             yolo_head_outs = self.yolo_head(neck_feats)
-            post_outs = self.yolo_head.post_process(yolo_head_outs, self.inputs)
+            post_outs = self.yolo_head.post_process(yolo_head_outs,
+                                                    self.inputs['im_shape'],
+                                                    self.inputs['scale_factor'])
 
             if not isinstance(post_outs, (tuple, list)):
                 # if set exclude_post_process, concat([pred_bboxes, pred_scores]) not scaled to origin
