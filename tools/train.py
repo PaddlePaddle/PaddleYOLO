ec2c3e11b53852b61c7c4a6e7f5a52fd593bd277
Theendlessofhell <148317258+Theendlessofhell@users.noreply.github.com>
2025-12-18 13:40:13 +0800

    Update train.py
    Added a conditional branch for metax_gpu

--- tools/train.py
+++ tools/train.py
@@ -168,11 +168,16 @@ def main():
     if 'use_gpu' not in cfg:
         cfg.use_gpu = False
 
+    if 'use_metax_gpu' not in cfg:
+        cfg.use_metax_gpu = False
+
     # disable mlu in config by default
     if 'use_mlu' not in cfg:
         cfg.use_mlu = False
 
     if cfg.use_gpu:
+        place = paddle.set_device('gpu')
+    elif cfg.use_metax_gpu:
         place = paddle.set_device('metax_gpu')
     elif cfg.use_npu:
         place = paddle.set_device('npu')
@@ -190,6 +195,7 @@ def main():
     merge_config(FLAGS.opt)
     check.check_config(cfg)
     check.check_gpu(cfg.use_gpu)
+    check.check_metax_gpu(cfg.use_metax_gpu)
     check.check_npu(cfg.use_npu)
     check.check_xpu(cfg.use_xpu)
     check.check_mlu(cfg.use_mlu)
